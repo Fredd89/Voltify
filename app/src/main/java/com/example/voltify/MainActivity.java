a722d466d388115adf8f95ff64acc4f15e0394c4
Fred <ficofedebla@gmail.com>
2022-03-14 23:44:39 +0100

    Manage file class implemented

--- app/src/main/java/com/example/voltify/MainActivity.java
+++ app/src/main/java/com/example/voltify/MainActivity.java
@@ -5,14 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import java.util.ArrayList;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ShowActivity.class);
                 String string = songManager.showSongs(context);
+                Log.d("Dio lupo", string);
                 intent.putExtra("songsList", string);
-                Toast.makeText(context, string, Toast.LENGTH_LONG).show();
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/voltify/ShowActivity.java
+++ app/src/main/java/com/example/voltify/ShowActivity.java
@@ -2,24 +2,51 @@ package com.example.voltify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShowActivity extends AppCompatActivity {
 
     EditText edtTxt;
+    Button importButton, exportButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
 
+        ManageFile manageFile = new ManageFile();
+        Context context = getApplicationContext();
         edtTxt = findViewById(R.id.edtTxt);
+        importButton = findViewById(R.id.importButton);
+        exportButton = findViewById(R.id.exportButton);
 
-        Intent intent = new Intent();
+        Intent intent = getIntent();
         String s = intent.getStringExtra("songsList");
+        edtTxt.setText(s);
+
+        importButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt = edtTxt.getText().toString();
+                manageFile.writeFile("Text.txt", txt, context);
+                Toast.makeText(context, "File correctly written", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
